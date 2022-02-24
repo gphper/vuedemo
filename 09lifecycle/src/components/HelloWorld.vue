cf05106d4a1c2db5f3c8e89346321eaa0666108a
gphper <570165887@qq.com>
2022-02-24 22:02:15 +0800

    组件生命周期

---
+++ 09lifecycle/src/components/HelloWorld.vue
@@ -0,0 +1,107 @@
+<!--
+ * @Description: 
+ * @Author: gphper
+ * @Date: 2022-02-24 20:47:02
+-->
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  beforeCreate(){
+    console.log("实例创建之前自动调用 beforeCreated()")
+  },
+
+  created(){
+    this.$nextTick(()=>{
+      console.log("延迟调用created()")
+    })
+    console.log("实例创建时调用created()")
+  },
+
+  beforeMount(){
+    console.log("编译模板之前调用beforeMount()")
+  },
+
+  mounted(){
+    console.log("编译模板时调用mounted()")
+  },
+
+  beforeUpdate(){
+    console.log("更新前调用beforeUpdate()")
+  },
+
+  updated(){
+    console.log("模板内容更新完成")
+  },
+
+  beforeUnmount(){
+    console.log("实例销毁之前调用")
+  },
+
+  unmounted(){
+    console.log("实例销毁完成时调用")
+  },
+
+  activated(){
+    console.log("缓存数据恢复之前调用")
+  },
+
+  deactivated(){
+    console.log("通过keep-alive缓存之前调用")
+  },
+
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
